41ae73da91778062fbb4c5f45771caa448163bb9
wuhuanhost <post.thinking@gmail.com>
2016-09-27 10:46:17 +0800

    update max_num

--- max_test/max_num.c
+++ max_test/max_num.c
@@ -1,16 +1,17 @@
 #include <stdio.h>  
-#include <stdlib.h>  
+#include <stdlib.h>
+#include <locale.h>
 #include "max.h"  
   
 int main(void){
-	printf("****************两个数比较大小*******************");
-	printf("请输入第一个数");
-	char *str1=scanf("%s",str);//定义字符串接收用户输入
-	int num1=atoi(str1);//用户输入的字符串转换为整型
-	printf("请输入第二个数");
-	char *str2=scanf("%s",str);//定义字符串接收用户输入
-	int num2=atoi(str2);//用户输入的字符串转换为整型
-    printf("The bigger one of 3 and 5 is %d\n", max(num1, num2));  
+	setlocale(LC_ALL, "chs");
+	int num1,num2;
+	printf("****************compare the two numbers*******************\n");
+	printf("please enter the first number:\n");
+	scanf("%d",&num1);//定义字符串接收用户输入
+	printf("please enter the second number:\n");
+	scanf("%d",&num2);//定义字符串接收用户输入
+    printf("The bigger number %d\n", max(num1, num2));  
     system("pause");  
     return 0;
 }
